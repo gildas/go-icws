9b2b7dc5f9698ac34a5eba06df1c5831c688aeb4
Gildas Cherruel <gildas@breizh.org>
2021-07-07 15:21:59 +0900

    more efficient unmarshaling

--- message.go
+++ message.go
@@ -1,8 +1,6 @@
 package icws
 
 import (
-	"encoding/json"
-
 	"github.com/gildas/go-core"
 	"github.com/gildas/go-errors"
 )
@@ -11,28 +9,13 @@ type Message interface {
 	core.TypeCarrier
 }
 
-type messageWrapper struct {
-	Message Message
-}
-
 var messageRegistry = core.TypeRegistry{}
 
 // UnmarshalMessage unmarshals from a JSON payload
 func UnmarshalMessage(payload []byte) (Message, error) {
-	wrapper := messageWrapper{}
-	if err := json.Unmarshal(payload, &wrapper); err != nil {
-		return nil, err // err is already decorated by the UnmarshalJSON funcs
-	}
-	return wrapper.Message, nil
-}
-
-// UnmarshalJSON unmarshals from JSON
-//
-// implements json.Unmarshaler
-func (wrapper *messageWrapper) UnmarshalJSON(payload []byte) (err error) {
 	value, err := messageRegistry.UnmarshalJSON(payload, "__type")
-	if err == nil {
-		wrapper.Message = value.(Message)
+	if err != nil {
+		return nil, errors.JSONUnmarshalError.Wrap(err)
 	}
-	return errors.WithStack(err)
+	return value.(Message), nil
 }
